7054ce96dcecebd643c66ae0822c8da91b1f9b75
Sneha <nehasvermaind@gmail.com>
2023-08-01 22:02:18 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+
+
+function Header(){
+return(
+       
+       <Navbar className="bg-body-tertiary" sticky="top">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="assets/images/logo/logo.png"
+              className="d-inline-block align-top"
+              alt="Maspes logo"
+            />
+          </Navbar.Brand>
+          <Nav className="links">
+          <Nav className="me-auto" >
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#Features">Features</Nav.Link>
+            <Nav.Link href="#Services">Services</Nav.Link>
+            <Nav.Link href="#Pricingplan">Pricing Plan</Nav.Link>
+            <Nav.Link href="#Why-Application">Why Application</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+            <Nav.Link href="#Contact-Us">Contact Us</Nav.Link>
+          </Nav>
+          </Nav>
+          <Button variant="success">Get the App</Button>{' '}
+        </Container>
+      </Navbar>
+       
+                  
+)
+}
+export default Header;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,79 @@
+import './Home.css';
+import {Button} from 'react-bootstrap/Button/Button/Button';
+
+function Home() {
+    return (
+        <div>
+            {/* <section className="hero-area hero-area-lg position-relative" id="home">
+                <div className="container">
+                    <div className="row banner_bg">
+                        <div className="col-lg-6 text-center text-lg-left mb-4 mb-lg-0">
+                            <h1 className="position-relative aos-init aos-animate" data-aos="zoom-in-up" data-aos-delay="600">
+                                Elevate Your Food &amp; Restaurant Business With 
+                               <img src="assets/images/feature/maspes.png" className="mt-3"/>
+                            </h1>
+                            <p className="position-relative h3 aos-init aos-animate" data-aos="zoom-in-up" data-aos-delay="800">An Ultimate Ordering And Delivery Solution
+                            </p>
+                            <h6 className="text-dark font-weight-normal aos-init" data-aos="zoom-in-up" data-aos-delay="1000">With food delivery app development, you can get a
+                            </h6>
+                            <p className="mb-4 aos-init" data-aos="zoom-in-up" data-aos-delay="1000">business-focused solution that will help your business succeed. Our dedicated 
+                            <br/>
+                            developers will work with you to ensure you have full control over your
+                             <br/>
+                              restaurant's business activities.</p>
+                            <ul className="list-inline">
+                                <li className="list-inline-item aos-init" data-aos="zoom-in" data-aos-delay="1500">
+                                    <a className="btn btn-white" href="#"><div {{style:"display: flex; flex-direction: row;"}}><a></div>
+                                        <div><i className="fa fa-apple mr-2"></i></div>
+                                <div>
+                            <div style="font-size: 10px; text-align: left;">
+                                Available on the 
+                            </div>
+                            <div className="banner-btn">
+                                App Store 
+                            </div>
+                        </div>
+                    
+                </a>
+                                </li>
+            <li className="list-inline-item mb-3 mb-lg-0 aos-init" data-aos="zoom-in" data-aos-delay="1600">
+                <a className="btn btn-white" href="#">
+                    <div {{style:"display: flex; flex-direction: row;"}}>
+                        <div>
+                            <img src="assets/images/icon/001-google-play.png" className="img-fluid mr-2" alt="googleplay"/>
+                        </div>
+                        <div>
+                            <div {{style:"font-size: 10px; text-align: left;"}}>
+                                Get it on
+                            </div>
+                            <div className="banner-btn">
+                                 Google Play
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </li>
+                            </ul>
+                        </div>
+    <div className="col-lg-6">
+        <img src="/static/media/banner.3d2cccd44c867e74beb64814eeb0d201.svg" className="img-fluid aos-init" data-aos="fade-left" data-aos-delay="1500"/>
+    </div>
+                    </div>
+                </div>
+        <img src="assets/images/shape/about_s4_lines 1.png" className="img-fluid bg-shape-4 aos-init aos-animate" alt="shape" data-aos="fade-left" data-aos-delay="1500"/>
+
+            </section> */}
+           
+           
+            <div class="container-sm" >
+            
+            </div>
+
+<Button/>        
+
+        </div>
+    )
+   
+}
+  
+export default Home;
